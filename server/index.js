752743887b4521c22eba019adae0664a473e6fbe
Jared Grippe <jared@deadlyicon.com>
2017-05-09 11:47:26 -0700

    WIP

--- server/index.js
+++ server/index.js
@@ -11,66 +11,68 @@ if (process.env.NODE_ENV === 'production') {
   server.use(https({trustProtoHeader: true}))
 }
 
+server.use(express.static(__dirname+'/public'))
 server.use(cookieParser())
 
 require('./authentication')(server)
 
 server.get('/', (req, res) => {
-  const redirectTo = encodeURIComponent(`${req.protocol}://${req.get('host')}${req.originalUrl}`)
-  const loginURL = `${process.env.IDM_BASE_URL}/sign-in?redirect=${redirectTo}`
-  res.send(`
-    <h1>Coachatron</h1>
-    <a href="${loginURL}">Login</a>
-    <a href="${process.env.IDM_BASE_URL}/auth/sign-out">Sign-out</a>
-    <pre>${JSON.stringify(req.user, null, 2)}</pre>
-  `)
+  res.sendFile(__dirname+'/public/index.html');
+  // const redirectTo = encodeURIComponent(`${req.protocol}://${req.get('host')}${req.originalUrl}`)
+  // const loginURL = `${process.env.IDM_BASE_URL}/sign-in?redirect=${redirectTo}`
+  // res.send(`
+  //   <h1>Coachatron</h1>
+  //   <a href="${loginURL}">Login</a>
+  //   <a href="${process.env.IDM_BASE_URL}/auth/sign-out">Sign-out</a>
+  //   <pre>${JSON.stringify(req.user, null, 2)}</pre>
+  // `)
 })
 
-server.get('/idm-api-test', (req, res, next) => {
-  const query = `
-    query ($id: ID!) {
-      getUserById(id: $id) {
-        id
-        email
-      }
-    }
-  `
-  req.queryIdm(query, {id: req.utser.id})
-    .then(results => {
-      res.json(results)
-    })
-    .catch(error => {
-      res.status(500).json({
-        error: error.message,
-        stack: error.stack,
-      })
-    })
-})
+// server.get('/idm-api-test', (req, res, next) => {
+//   const query = `
+//     query ($id: ID!) {
+//       getUserById(id: $id) {
+//         id
+//         email
+//       }
+//     }
+//   `
+//   req.queryIdm(query, {id: req.utser.id})
+//     .then(results => {
+//       res.json(results)
+//     })
+//     .catch(error => {
+//       res.status(500).json({
+//         error: error.message,
+//         stack: error.stack,
+//       })
+//     })
+// })
 
-server.get('/cycledata', (req, res, next) => {
-  const query = `
-    query {
-      findProjects {
-        id
-        name
-        artifactURL
-        cycle {
-          cycleNumber
-        }
-      }
-    }
-  `
-  req.queryGame(query, {})
-    .then(results => {
-      res.json(results)
-    })
-    .catch(error => {
-      res.status(500).json({
-        error: error.message,
-        stack: error.stack,
-      })
-    })
-})
+// server.get('/cycledata', (req, res, next) => {
+//   const query = `
+//     query {
+//       findProjects {
+//         id
+//         name
+//         artifactURL
+//         cycle {
+//           cycleNumber
+//         }
+//       }
+//     }
+//   `
+//   req.queryGame(query, {})
+//     .then(results => {
+//       res.json(results)
+//     })
+//     .catch(error => {
+//       res.status(500).json({
+//         error: error.message,
+//         stack: error.stack,
+//       })
+//     })
+// })
 
 server.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`)
